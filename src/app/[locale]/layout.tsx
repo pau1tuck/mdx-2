3c20ed96d4fda258673b5ce4c040c436b98b0c12
Paul Tuck <paul.tuck@oxon.org>
2024-03-22 04:49:23 +0700

    fix(eslint): correct eslintrc.json indentation

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,58 +1,67 @@
-import '@/styles/global.css';
+import "@/styles/global.css";
 
-import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { Button } from "flowbite-react";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider, useMessages } from "next-intl";
 
-import { AppConfig } from '@/utils/AppConfig';
+import { AppConfig } from "@/utils/AppConfig";
 
 export const metadata: Metadata = {
-  icons: [
-    {
-      rel: 'apple-touch-icon',
-      url: '/apple-touch-icon.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon-32x32.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon-16x16.png',
-    },
-    {
-      rel: 'icon',
-      url: '/favicon.ico',
-    },
-  ],
+    icons: [
+        {
+            rel: "apple-touch-icon",
+            url: "/apple-touch-icon.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            url: "/favicon-32x32.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            url: "/favicon-16x16.png",
+        },
+        {
+            rel: "icon",
+            url: "/favicon.ico",
+        },
+    ],
 };
 
+const figtree = Figtree({
+    subsets: ["latin"],
+    variable: "--font-figtree",
+    display: "swap",
+});
+
 export default function RootLayout(props: {
-  children: React.ReactNode;
-  params: { locale: string };
+    children: React.ReactNode;
+    params: { locale: string };
 }) {
-  // Validate that the incoming `locale` parameter is valid
-  if (!AppConfig.locales.includes(props.params.locale)) notFound();
+    // Validate that the incoming `locale` parameter is valid
+    if (!AppConfig.locales.includes(props.params.locale)) notFound();
 
-  // Using internationalization in Client Components
-  const messages = useMessages();
+    // Using internationalization in Client Components
+    const messages = useMessages();
 
-  return (
-    <html lang={props.params.locale}>
-      <body>
-        <NextIntlClientProvider
-          locale={props.params.locale}
-          messages={messages}
-        >
-          {props.children}
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang={props.params.locale} className={figtree.className}>
+            <body>
+                <NextIntlClientProvider
+                    locale={props.params.locale}
+                    messages={messages}
+                >
+                    <Button>Hello!</Button>
+                    {props.children}
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
 }
 
 // Enable edge runtime but you are required to disable the `migrate` function in `src/libs/DB.ts`
